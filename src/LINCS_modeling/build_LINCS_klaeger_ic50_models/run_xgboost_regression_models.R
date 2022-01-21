513f37177f6b11a57a2de77a5e714cdfc394f023
cujoisa <cujoisa@live.unc.edu>
2022-01-21 15:01:29 -0500

    updating xgboost code

--- src/LINCS_modeling/build_LINCS_klaeger_ic50_models/run_xgboost_regression_models.R
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/run_xgboost_regression_models.R
@@ -11,28 +11,28 @@ doParallel::registerDoParallel()
 all_data_filtered = read_csv(here('results/all_model_data_filtered.csv'))
 all_data_feat_cors =  read_csv(here('results/all_filtered_data_feature_correlations.csv'))
 
-build_all_data_classification_viability_set = function(num_features, all_data, feature_correlations) {
+build_all_data_regression_viability_set = function(num_features, all_data, feature_correlations) {
 	this_data_filtered = all_data %>%
-		mutate(ic50_binary = as.factor(ic50_binary)) %>% 
+		mutate(ic50 = as.factor(ic50)) %>% 
 		select(any_of(feature_correlations$feature[1:num_features]),
 					 depmap_id,
 					 ccle_name,
 					 ic50,
 					 broad_id,
-					 ic50_binary)
+					 ic50)
 }
 
-get_all_data_classification_cv_metrics = function(features, data) {
-	this_dataset = build_all_data_classification_viability_set(feature_cor =  all_data_feat_cors,
+get_all_data_regression_cv_metrics = function(features, data) {
+	this_dataset = build_all_data_regression_viability_set(feature_cor =  all_data_feat_cors,
 																														 num_features = features,
 																														 all_data = data)
 	
 	folds = vfold_cv(this_dataset, v = 10)
 	
-	this_recipe = recipe(ic50_binary ~ ., this_dataset) %>%
+	this_recipe = recipe(ic50 ~ ., this_dataset) %>%
 		update_role(-starts_with("act_"),
 								-starts_with("exp_"),
-								-starts_with("ic50_binary"),
+								-starts_with("ic50"),
 								new_role = "id variable")
 	
 	xgb_spec <- boost_tree(
@@ -43,7 +43,7 @@ get_all_data_classification_cv_metrics = function(features, data) {
 		learn_rate = tune(),                         ## step size
 	) %>% 
 		set_engine("xgboost") %>% 
-		set_mode("classification")
+		set_mode("regression")
 	
 	xgb_grid <- grid_latin_hypercube(
 		tree_depth(),
@@ -69,19 +69,19 @@ get_all_data_classification_cv_metrics = function(features, data) {
 		control = control_grid(save_pred = TRUE)
 	)
 	
-	cv_metrics_classification = collect_metrics(fit)
+	cv_metrics_regression = collect_metrics(fit)
 	
-	return(cv_metrics_classification)
+	return(cv_metrics_regression)
 	
 }
 
 feature_list = c(250, 500, 1000 , 1500, 2000, 2500, 3000, 3500)
 
-all_data_classification_metrics = data.frame()
+all_data_regression_metrics = data.frame()
 for (i in 1:length(feature_list)) {
-	this_metrics = get_all_data_classification_cv_metrics(features = feature_list[i], data = all_data_filtered) %>%
+	this_metrics = get_all_data_regression_cv_metrics(features = feature_list[i], data = all_data_filtered) %>%
 		mutate(feature_number = feature_list[i])
-	all_data_classification_metrics = bind_rows(all_data_classification_metrics, this_metrics)
+	all_data_regression_metrics = bind_rows(all_data_regression_metrics, this_metrics)
 }
 
-write_csv(all_data_classification_metrics, here('results/klaeger_LINCS_xgboost_classification_results.csv'))
+write_csv(all_data_regression_metrics, here('results/klaeger_LINCS_xgboost_regression_results.csv'))
