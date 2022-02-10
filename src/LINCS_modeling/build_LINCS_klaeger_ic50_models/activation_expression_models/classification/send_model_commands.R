60b8ac42b93ca66e5cc4c5f62659addfa5e8aa69
cujoisa <cujoisa@live.unc.edu>
2022-02-10 14:50:14 -0500

    Update send_model_commands.R

--- src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/send_model_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/send_model_commands.R
@@ -1,7 +1,7 @@
 library(here)
 library(tidyverse)
 
-# for (feature_num in c(5)) {
+# for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 # 
 # 		job_name = sprintf('RF_%d',feature_num)
 # 		
@@ -12,7 +12,7 @@ library(tidyverse)
 # 	
 # }
 
-# for (feature_num in c(5)) {
+# for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 # 	
 # 	job_name = sprintf('xbg_%d',feature_num)
 # 	
@@ -24,12 +24,12 @@ library(tidyverse)
 # }
 
 
-for (feature_num in c(5)) {
+for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 	
 	
 	job_name = sprintf('NN_%d',feature_num)
 	
-	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=80G --time=120:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/build_NN_models.R --feature_num %d"', job_name, feature_num)
+	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=90G --time=120:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/build_NN_models.R --feature_num %d"', job_name, feature_num)
 	
 	# print(command)
 	system(command)
