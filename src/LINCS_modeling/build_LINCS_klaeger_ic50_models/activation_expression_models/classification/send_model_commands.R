1f3dccda785e458a108a6eb45369ae8ee24f0857
cujoisa <cujoisa@live.unc.edu>
2022-02-22 12:43:16 -0500

    Update send_model_commands.R

--- src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/send_model_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/send_model_commands.R
@@ -16,7 +16,7 @@ for (feature_num in c(10)) {
 
 	job_name = sprintf('xbg_%d',feature_num)
 
-	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=90G --time=190:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/build_NN_models.R --feature_num %d"', job_name, feature_num)
+	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=90G --time=1:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/classification/build_xgboost_models_ANOVA.R --feature_num %d"', job_name, feature_num)
 
 	# print(command)
 	system(command)
