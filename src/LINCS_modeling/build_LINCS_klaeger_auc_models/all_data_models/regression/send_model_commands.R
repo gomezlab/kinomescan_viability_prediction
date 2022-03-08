0847023f5ddf2c66bac4876ba6e62047b06acaea
cujoisa <cujoisa@live.unc.edu>
2022-03-08 17:29:46 -0500

    adding figure 2 code

--- src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/send_model_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/send_model_commands.R
@@ -1,18 +1,18 @@
 library(here)
 library(tidyverse)
 
-for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
-
-		job_name = sprintf('auc_aRF%d',feature_num)
-
-		command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=120:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
-
-		# print(command)
-		system(command)
-
-}
-
-for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
+# for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
+# 
+# 		job_name = sprintf('auc_aRF%d',feature_num)
+# 
+# 		command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=120:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
+# 
+# 		# print(command)
+# 		system(command)
+# 
+# }
+# 
+for (feature_num in c(10)) {
 
 	job_name = sprintf('auc_axgb%d',feature_num)
 
