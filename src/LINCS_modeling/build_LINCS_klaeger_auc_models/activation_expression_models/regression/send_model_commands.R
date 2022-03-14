d51669d756b69dbe75555ce545fef3bf09c8fd0d
cujoisa <cujoisa@live.unc.edu>
2022-03-14 15:31:01 -0400

    updates to send model code

--- src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/send_model_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/send_model_commands.R
@@ -1,27 +1,27 @@
 library(here)
 library(tidyverse)
 
-# for (feature_num in c(100)) {
-# 
-# 		job_name = sprintf('aucRF%d',feature_num)
-# 
-# 	command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=40:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
-# 
-# 		# print(command)
-# 		system(command)
-# 
-# }
+for (feature_num in c(200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 
-# for (feature_num in c(100)) {
-# 
-# 	job_name = sprintf('auclr%d',feature_num)
-# 
-# 	command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=40:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/build_lr_models_ANOVA.R --feature_num %d"', job_name, feature_num)
-# 
-# 	# print(command)
-# 	system(command)
-# 
-# }
+		job_name = sprintf('aucRF%d',feature_num)
+
+	command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=40:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
+
+		# print(command)
+		system(command)
+
+}
+
+for (feature_num in c(200,300,400,500,1000,1500,2000,3000,4000,5000)) {
+
+	job_name = sprintf('auclr%d',feature_num)
+
+	command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=40:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/build_lr_models_ANOVA.R --feature_num %d"', job_name, feature_num)
+
+	# print(command)
+	system(command)
+
+}
 # 
 # for (feature_num in c(200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 # 
