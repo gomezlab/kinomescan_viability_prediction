6f31c34ae12620f960d61ade9ae250b00524cbbb
cujoisa <cujoisa@live.unc.edu>
2022-03-14 15:33:04 -0400

    Update send_model_commands.R

--- src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/send_model_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/send_model_commands.R
@@ -16,7 +16,7 @@ for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 
 		job_name = sprintf('RF%d',feature_num)
 
-		command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=120:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
+		command = sprintf('sbatch --job-name=%s --mem=99G -c 16 --time=40:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
 
 		# print(command)
 		system(command)
