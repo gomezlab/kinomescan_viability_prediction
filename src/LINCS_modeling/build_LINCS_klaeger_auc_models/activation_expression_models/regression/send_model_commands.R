f263056183c35f5b4dd3db660a0992b06c4c0b30
cujoisa <cujoisa@live.unc.edu>
2022-03-14 11:31:08 -0400

    corrections

--- src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/send_model_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/send_model_commands.R
@@ -23,19 +23,19 @@ library(tidyverse)
 # 
 # }
 # 
-# for (feature_num in c(200,300,400,500,1000,1500,2000,3000,4000,5000)) {
-# 
-# 	job_name = sprintf('aucxg%d',feature_num)
-# 
-# 	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=90G --time=90:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/build_xgboost_models_ANOVA_GPU.R --feature_num %d"', job_name, feature_num)
-# 
-# 	# print(command)
-# 	system(command)
-# 
-# }
-# 
+for (feature_num in c(200,300,400,500,1000,1500,2000,3000,4000,5000)) {
+
+	job_name = sprintf('aucxg%d',feature_num)
+
+	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=90G --time=90:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/build_xgboost_models_ANOVA_GPU.R --feature_num %d"', job_name, feature_num)
+
+	# print(command)
+	system(command)
+
+}
+
 # #,200,300,400,500,1000,1500,2000,3000,4000,5000
-for (feature_num in c(200)) {
+for (feature_num in c(300,400,500,1000,1500,2000,3000,4000,5000)) {
 
 
 	job_name = sprintf('aucNN%d',feature_num)
