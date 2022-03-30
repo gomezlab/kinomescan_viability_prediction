ce5ed40e497e7bfe5f24022c072a7366cc90be1c
cujoisa <cujoisa@live.unc.edu>
2022-03-30 17:59:48 -0400

    adding lasso feature selection code

---
+++ src/LINCS_modeling/klaeger_only_models/auc/lasso_feature_selection/lasso_feature_selection.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(here)
+library(vroom)
+library(tidymodels)
+library(finetune)
+library(tictoc)
+library(recipeselectors)
+library(doParallel)
+library(patchwork)
+library(ROCR)
+library(vip)
+
+this_dataset = read_rds(here('results/klaeger_only_models_auc/PRISM_klaeger_only_data_5000feat_auc.rds.gz'))
+cors =  vroom(here('results/PRISM_LINCS_klaeger_models_auc/PRISM_LINCS_klaeger_data_feature_correlations_auc.csv'))
+
+lr_recipe = recipe(auc_target ~ ., this_dataset) %>%
+	update_role(-starts_with("act_"),
+							-starts_with("exp_"),
+							-starts_with("auc"),
+							new_role = "id variable") %>%
+	step_select(depmap_id,
+							ccle_name,
+							auc_target,
+							broad_id,
+							any_of(cors$feature[1:5000])) %>%
+	step_zv(all_predictors()) %>% 
+	step_normalize(all_predictors())
+
+lr_final_spec = linear_reg(penalty = 0.00001, mixture = 1) %>%
+	set_engine("glmnet") %>% 
+	set_mode("regression")
+
+final_wflow <-
+	workflow() %>%
+	add_model(lr_final_spec) %>%
+	add_recipe(lr_recipe)
+
+final_results = 
+	final_wflow %>% 
+	fit(this_dataset) %>% 
+	write_rds(here('results/klaeger_only_models_auc/5000feat_lasso_final_results.rds.gz'), compress = "gz")
+
+all_importance = vi(final_results %>% extract_fit_parsnip()) %>%
+	arrange(desc(Importance)) %>%
+	filter(Importance > 0) %>% 
+	mutate(rank = 1:n()) %>%
+	write_csv(here('results/klaeger_only_models_auc/5000feat_lasso_selected_features.csv'))
---
+++ src/LINCS_modeling/klaeger_only_models/auc/lasso_feature_selection/lasso_tuning.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(here)
+library(vroom)
+library(tidymodels)
+library(finetune)
+library(tictoc)
+library(recipeselectors)
+library(doParallel)
+library(patchwork)
+library(ROCR)
+library(vip)
+
+this_dataset = read_rds(here('results/klaeger_only_models_auc/PRISM_klaeger_only_data_5000feat_auc.rds.gz'))
+cors =  vroom(here('results/PRISM_LINCS_klaeger_models_auc/PRISM_LINCS_klaeger_data_feature_correlations_auc.csv'))
+
+folds = read_rds(here(here('results/cv_folds/PRISM_klaeger_only_folds_auc.rds.gz')))
+
+lr_recipe = recipe(auc_target ~ ., this_dataset) %>%
+	update_role(-starts_with("act_"),
+							-starts_with("exp_"),
+							-starts_with("auc"),
+							new_role = "id variable") %>%
+	step_select(depmap_id,
+							ccle_name,
+							auc_target,
+							broad_id,
+							any_of(cors$feature[1:5000])) %>%
+	step_zv(all_predictors()) %>% 
+	step_normalize(all_predictors())
+
+lr_spec <- linear_reg(penalty = tune(), mixture = 1) %>%
+	set_engine("glmnet") %>% 
+	set_mode("regression")
+
+lr_grid = grid_max_entropy(penalty(), size = 20)
+
+this_wflow <-
+	workflow() %>%
+	add_model(lr_spec) %>%
+	add_recipe(lr_recipe)
+
+race_ctrl = control_grid(
+	save_pred = TRUE, 
+	parallel_over = "everything",
+	verbose = TRUE
+)
+
+results = tune_grid(
+	this_wflow,
+	resamples = folds,
+	grid = lr_grid,
+	control = race_ctrl
+) %>% 
+	write_rds(here('results/klaeger_only_models_auc/5000feat_lasso_tuning_results.rds.gz'), compress = "gz")
+
+temp = collect_metrics(results)
+
+
