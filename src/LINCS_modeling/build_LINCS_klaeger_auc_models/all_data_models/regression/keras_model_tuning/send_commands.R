09f596d3b54d6cae9d5bea8057c25d5da6393580
cujoisa <cujoisa@live.unc.edu>
2022-04-19 11:38:09 -0400

    Update send_commands.R

--- src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/keras_model_tuning/send_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/keras_model_tuning/send_commands.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 for (feature_num in c(500,1000,1500,2000,3000,4000,5000)) {
 	
-	job_name = sprintf('1_%d',feature_num)
+	job_name = sprintf('1a%d',feature_num)
 	
 	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=50G --time=90:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/keras_model_tuning/one_layer.R --feature_num %d"', job_name, feature_num)
 	
@@ -15,7 +15,7 @@ for (feature_num in c(500,1000,1500,2000,3000,4000,5000)) {
 
 for (feature_num in c(1500,2000,3000,4000,5000)) {
 	
-	job_name = sprintf('2_%d',feature_num)
+	job_name = sprintf('2a%d',feature_num)
 	
 	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=50G --time=90:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/keras_model_tuning/two_layer.R --feature_num %d"', job_name, feature_num)
 	
@@ -26,7 +26,7 @@ for (feature_num in c(1500,2000,3000,4000,5000)) {
 
 for (feature_num in c(1500,2000,3000,4000,5000)) {
 	
-	job_name = sprintf('4_%d',feature_num)
+	job_name = sprintf('4a%d',feature_num)
 	
 	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=50G --time=90:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/keras_model_tuning/four_layer.R --feature_num %d"', job_name, feature_num)
 	
@@ -37,7 +37,7 @@ for (feature_num in c(1500,2000,3000,4000,5000)) {
 
 for (feature_num in c(2000,3000,4000,5000)) {
 	
-	job_name = sprintf('6_%d',feature_num)
+	job_name = sprintf('6a%d',feature_num)
 	
 	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=50G --time=90:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/keras_model_tuning/six_layer.R --feature_num %d"', job_name, feature_num)
 	
