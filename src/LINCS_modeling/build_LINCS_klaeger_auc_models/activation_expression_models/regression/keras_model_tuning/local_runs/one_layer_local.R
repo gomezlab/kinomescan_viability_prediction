208df316a0188e2f5848bf7cb292194bbdcf1097
cujoisa <cujoisa@live.unc.edu>
2022-05-03 14:03:53 -0400

    adding code to work on redhat 8

--- src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/keras_model_tuning/local_runs/one_layer_local.R
+++ src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/keras_model_tuning/local_runs/one_layer_local.R
@@ -24,7 +24,7 @@ parser <- ArgumentParser(description='Process input paramters')
 parser$add_argument('--feature_num', default = 100, type="integer")
 
 #args = parser$parse_args()
-args = data.frame(feature_num = c(4000,5000)) 
+args = data.frame(feature_num = c(5000)) 
 
 dropout = c(0.2,0.4,0.6)
 neurons = c(200,400,600,800,1000)
