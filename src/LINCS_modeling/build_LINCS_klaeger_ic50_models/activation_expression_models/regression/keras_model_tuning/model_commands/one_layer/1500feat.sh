ea0eae9c2b643b1c4993f82548f686ad8c0030ce
cujoisa <cujoisa@live.unc.edu>
2022-05-05 09:49:10 -0400

    write model command files

---
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/keras_model_tuning/model_commands/one_layer/1500feat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p volta-gpu
+#SBATCH -t 120:00:00
+#SBATCH --qos gpu_access
+#SBATCH --mem=30G
+#SBATCH --gres=gpu:1
+#SBATCH --constraint='rhel8'
+#SBATCH --job-name=1i1500
+module load r/4.1.0
+module load gcc/9.1.0
+module load cuda/11.4
+Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/keras_model_tuning/one_layer.R --feature_num 1500
---
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/all_data_models/regression/keras_model_tuning/model_commands/two_layer/4000feat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p volta-gpu
+#SBATCH -t 120:00:00
+#SBATCH --qos gpu_access
+#SBATCH --mem=30G
+#SBATCH --gres=gpu:1
+#SBATCH --constraint='rhel8'
+#SBATCH --job-name=4i4000
+module load r/4.1.0
+module load gcc/9.1.0
+module load cuda/11.4
+Rscript src/LINCS_modeling/build_LINCS_klaeger_auc_models/all_data_models/regression/keras_model_tuning/two_layer.R --feature_num 4000
