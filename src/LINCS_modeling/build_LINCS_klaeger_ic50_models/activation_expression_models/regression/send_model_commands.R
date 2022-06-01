873e4a3bfd2ebea1f5f910f143c99a3e5511c908
cujoisa <cujoisa@live.unc.edu>
2022-06-01 15:00:08 -0400

    updating send model command code

--- src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/send_model_commands.R
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/send_model_commands.R
@@ -1,16 +1,16 @@
 library(here)
 library(tidyverse)
 
-for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
-
-		job_name = sprintf('iRF%d',feature_num)
-
-	command = sprintf('sbatch --job-name=%s --mem=90G -c 16 --time=40:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
-
-		# print(command)
-		system(command)
-
-}
+# for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
+# 
+# 		job_name = sprintf('iRF%d',feature_num)
+# 
+# 	command = sprintf('sbatch --job-name=%s --mem=90G -c 16 --time=40:00:00 --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/build_rf_models_ANOVA.R --feature_num %d"', job_name, feature_num)
+# 
+# 		# print(command)
+# 		system(command)
+# 
+# }
 # 
 # for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 # 
@@ -27,7 +27,7 @@ for (feature_num in c(100,200,300,400,500,1000,1500,2000,3000,4000,5000)) {
 
 	job_name = sprintf('ixg%d',feature_num)
 
-	command = sprintf('sbatch -N 1 -n 1 -p volta-gpu --job-name=%s --mem=90G --time=48:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/build_xgboost_models_ANOVA_GPU.R --feature_num %d"', job_name, feature_num)
+	command = sprintf('sbatch -N 1 -n 1 -p gpu --job-name=%s --mem=90G --time=48:00:00 --qos gpu_access --gres=gpu:1 --mail-user=cujoisa@live.unc.edu   --wrap "Rscript src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/build_xgboost_models_ANOVA_GPU.R --feature_num %d"', job_name, feature_num)
 
 	# print(command)
 	system(command)
