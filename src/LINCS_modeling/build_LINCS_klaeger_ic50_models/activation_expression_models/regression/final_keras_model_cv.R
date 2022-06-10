2fbc771889a8330ed03792eabf6abb4d72d17710
cujoisa <cujoisa@live.unc.edu>
2022-06-10 17:16:54 -0400

    updating prediction set

--- src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/final_keras_model_cv.R
+++ src/LINCS_modeling/build_LINCS_klaeger_ic50_models/activation_expression_models/regression/final_keras_model_cv.R
@@ -13,22 +13,13 @@ conflict_prefer("filter", "dplyr")
 conflict_prefer("all_numeric", "tfdatasets")
 conflict_prefer("rmse", "Metrics")
 
-compound_match_list = read_csv(here('results/matching/LINCS_PRISM_klaeger_combined_drug_matches_final.csv'))
 data = read_rds(here('results/PRISM_LINCS_klaeger_models_ic50/PRISM_LINCS_klaeger_data_for_ml_5000feat_ic50.rds.gz'))
 cors =  vroom(here('results/PRISM_LINCS_klaeger_models_ic50/PRISM_LINCS_klaeger_data_feature_correlations_ic50.csv'))
-args = data.frame(feature_num = c(2000))
+args = data.frame(feature_num = c(5000))
 
-i = 1
-# for(i in 1:length(args$feature_num)) {
+
 tic()	
-	
-dir.create(here('results/PRISM_LINCS_klaeger_models_ic50/activation_expression/regression/', 
-									sprintf('keras/results')), 
-						 showWarnings = F, recursive = T)
-	
-full_output_file = here('results/PRISM_LINCS_klaeger_models_ic50/activation_expression/regression/keras/results', 
-													sprintf('%dfeat.csv',args$feature_num[i]))
-
+i = 1
 this_dataset = data %>% 
 	rename('ic50_target' = ic50) %>% 
 	select(depmap_id,
@@ -44,8 +35,6 @@ spec = feature_spec(this_dataset %>%
 										ic50_target ~ .) %>%
 	step_numeric_column(all_numeric()) %>%
 	fit()
-
-# split = initial_split(this_dataset)
 
 set.seed(2222)
 folds = vfold_cv(this_dataset, v = 10)
@@ -67,13 +56,13 @@ main_output = input %>%
 	layer_dense_features(dense_features(spec)) %>%
 	layer_batch_normalization() %>% 
 	layer_dense(units = 200, activation = 'relu') %>% 
-	layer_dropout(rate = 0.2) %>% 
+	layer_dropout(rate = 0.4) %>% 
 	layer_dense(units = 200, activation = 'relu') %>% 
-	layer_dropout(rate = 0.2) %>% 
+	layer_dropout(rate = 0.4) %>% 
 	layer_dense(units = 200, activation = 'relu') %>% 
-	layer_dropout(rate = 0.2) %>% 
+	layer_dropout(rate = 0.4) %>% 
 	layer_dense(units = 200, activation = 'relu') %>% 
-	layer_dropout(rate = 0.2) %>% 
+	layer_dropout(rate = 0.4) %>% 
 	layer_dense(units = 1, activation = 'linear')
 
 model = keras_model(
@@ -87,16 +76,9 @@ model %>%
 		loss = 'mse',
 		metrics= metric_root_mean_squared_error())
 
-# print_dot_callback <- callback_lambda(
-# 	on_epoch_end = function(epoch, logs) {
-# 		if (epoch %% 80 == 0) cat("\n")
-# 		cat(".")
-# 	}
-# )  
-
 early_stop <- callback_early_stopping(monitor = "val_loss", 
-																			patience = 25,
-																			min_delta = 0.000006,
+																			patience = 20,
+																			min_delta = 0.00001,
 																			restore_best_weights = TRUE)
 set.seed(2222)
 history <- model %>% fit(
@@ -142,7 +124,7 @@ all_predictions = bind_rows(all_predictions, predictions)
 toc()
 
 k_clear_session()
-# }
+
 }
 
 write_csv(all_metrics, 
