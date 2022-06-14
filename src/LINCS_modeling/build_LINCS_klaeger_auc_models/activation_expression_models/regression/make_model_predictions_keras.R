2b3dc57800cb12886a5c44b5c0fe1a600adbf392
cujoisa <cujoisa@live.unc.edu>
2022-06-14 16:05:21 -0400

    Update make_model_predictions_keras.R

---
+++ src/LINCS_modeling/build_LINCS_klaeger_auc_models/activation_expression_models/regression/make_model_predictions_keras.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(here)
+library(keras)
+
+model = load_model_hdf5(here('results/PRISM_LINCS_klaeger_models_auc/activation_expression/regression/keras/final_keras_model.h5'))
+
+not_tested_data = read_rds(here('results/PRISM_LINCS_klaeger_models_auc/activation_expression/regression/not_tested_data.rds.gz'))
+
+
+tic()
+predictions <- model %>% 
+	predict(not_tested_data %>% 
+						select(starts_with(c("act_", "exp_")))) %>% 
+	write_rds(here('results/PRISM_LINCS_klaeger_models_auc/final_model_predictions.rds'))
+
+toc()
+
+ids = not_tested_data %>% select(-starts_with(c("act_", "exp_")))
+
+full_predictions = data.frame(predicted_auc = predictions) %>% 
+	bind_cols(ids) %>% 
+	write_csv(here('results/PRISM_LINCS_klaeger_models_auc/final_model_predictions_keras.csv'))
